de5549e3bdd3c92537510bde7a7eeec03fbe3ed8
yuriiQA5 <divan14391@gmail.com>
2023-07-01 13:00:35 +0300

    HomeWork

---
+++ src/HomeWork.java
@@ -0,0 +1,16 @@
+package src;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 9;
+        a-=b;
+        b= b+a;
+        a= b-a ;
+        System.out.println("a=");
+        System.out.println(a);
+        System.out.println("b=");
+        System.out.println(b);
+    }
+}
