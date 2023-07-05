3fb3f2adb70e6bfe4f582349cd72fec071f3cff2
yuriiQA5 <divan14391@gmail.com>
2023-07-05 19:53:54 +0300

    homrwork2 update

--- src/homeWork/HomeWork2.java
+++ src/homeWork/HomeWork2.java
@@ -15,31 +15,32 @@ public class HomeWork2 {
         System.out.println("===============");
 
         String string = "Testing, is my favourite job";
-        String[] splitedWords = string.split("[\\s,]");
-        System.out.println(Arrays.toString(splitedWords));
-        int firstWordLength = splitedWords[0].length();
-        Boolean result = true;
-        for (int i = 0; i<splitedWords.length ; i++)
-        {
-            System.out.println("слово " + i+" = " + splitedWords[i] + ", Длинна этого слова = " + splitedWords[i].length());
-            if (result)
-                result = firstWordLength > splitedWords[i].length();
-        }
-
-
-        System.out.println(result);
-//        String word1 = string.substring(0, 7);
-//        String word2 = string.substring(9, 11);
-//        String word3 = string.substring(12, 14);
-//        String word4 = string.substring(15, 24);
-//        String word5 = string.substring(25, 28);
-//        System.out.println(word1.concat("=").concat(String.valueOf(word1.length())));
-//        System.out.println(word2.concat("=").concat(String.valueOf(word2.length())));
-//        System.out.println(word3.concat("=").concat(String.valueOf(word3.length())));
-//        System.out.println(word4.concat("=").concat(String.valueOf(word4.length())));
-//        System.out.println(word5.concat("=").concat(String.valueOf(word5.length())));
-//        boolean result = word1.length()>word2.length() && word1.length()>word3.length() && word1.length()>word3.length() && word1.length()>word4.length() && word1.length()>word5.length();
+//        String[] splitedWords = string.split("[\\s,]");
+//        System.out.println(Arrays.toString(splitedWords));
+//        int firstWordLength = splitedWords[0].length();
+//        Boolean result = true;
+//        for (int i = 0; i<splitedWords.length ; i++)
+//        {
+//            System.out.println("слово " + i+" = " + splitedWords[i] + ", Длинна этого слова = " + splitedWords[i].length());
+//            if (result)
+//                result = firstWordLength > splitedWords[i].length();
+//        }
+//
+//
 //        System.out.println(result);
+        String word1 = string.split("\\s")[0];
+        word1 = word1.replace(",","");
+        String word2 = string.split("\\s")[1];
+        String word3 = string.split("\\s")[2];
+        String word4 = string.split("\\s")[3];
+        String word5 = string.split("\\s")[4];
+        System.out.println("слово1 " + word1 + ", Длинна этого слова =" + word1.length() + ".");
+        System.out.println("слово2 " + word2 + ", Длинна этого слова =" + word2.length() + ".");
+        System.out.println("слово3 " + word3 + ", Длинна этого слова =" + word3.length() + ".");
+        System.out.println("слово4 " + word4 + ", Длинна этого слова =" + word4.length() + ".");
+        System.out.println("слово5 " + word5 + ", Длинна этого слова =" + word5.length() + ".");
+        boolean result = word1.length()>word2.length() && word1.length()>word3.length() && word1.length()>word3.length() && word1.length()>word4.length() && word1.length()>word5.length();
+        System.out.println(result);
         System.out.println("=============");
 
         String someText = "Completely random text in English. In it, we just need to determine how many times the character 'a' occurs there. And we can use the split method and the length method.";
